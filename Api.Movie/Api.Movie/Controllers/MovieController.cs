3605d0f89463fd17a533e99556cdfc6958c7122a
Gabriel Silva <gsilva2510@hotmail.com>
2020-06-02 20:24:46 -0300

    modifing app settings

---
+++ Api.Movie/Api.Movie/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Movie.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+    }
+}
